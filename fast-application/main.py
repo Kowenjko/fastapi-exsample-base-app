0f947a86e47ab6ab302ddc81b1b1f1e8ba7aa6bc
Kowenjko <ortoswt@gmail.com>
2025-11-08 15:37:41 +0200

    add settings env

--- fast-application/main.py
+++ fast-application/main.py
@@ -1,19 +1,49 @@
+from typing import AsyncGenerator
 import uvicorn
 from fastapi import FastAPI
 from api import router as api_router
 from core.config import settings
 
-app = FastAPI()
+from contextlib import asynccontextmanager
+
+from core.models import db_helper
 
 
-app.include_router(api_router, prefix=settings.api.prefix)
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    # # startup
+    # if not broker.is_worker_process:
+    #     await broker.startup()
+
+    # FastStream broker
+    # await broker.start()
+
+    yield
+    # shutdown
+    print("dispose engine")
+
+    await db_helper.dispose()
+
+    # FastStream broker
+    # await broker.stop()
+
+    # if not broker.is_worker_process:
+    #     await broker.shutdown()
 
 
-@app.get("/")
+main_app = FastAPI(lifespan=lifespan)
+
+
+main_app.include_router(api_router, prefix=settings.api.prefix)
+
+
+@main_app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
 if __name__ == "__main__":
-    # uvicorn.run("main:app", reload=True)
-    uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
+
+    uvicorn.run(
+        "main:main_app", host=settings.run.host, port=settings.run.port, reload=True
+    )
