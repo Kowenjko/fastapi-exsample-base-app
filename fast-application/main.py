80664d9e86880f1b00c87078971baefc4fc7b6b9
Kowenjko <ortoswt@gmail.com>
2025-11-08 12:00:18 +0200

    add unicorn add load by new host port

--- fast-application/main.py
+++ fast-application/main.py
@@ -1,8 +1,10 @@
+import uvicorn
 from fastapi import FastAPI
 from api import router as api_router
 from core.config import settings
 
 app = FastAPI()
+
 
 app.include_router(api_router, prefix=settings.api.prefix)
 
@@ -10,3 +12,8 @@ app.include_router(api_router, prefix=settings.api.prefix)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    # uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
