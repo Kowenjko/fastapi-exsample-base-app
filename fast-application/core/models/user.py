e36fb3856732a5baa346f388d6ed898db3404193
Kowenjko <ortoswt@gmail.com>
2025-12-04 16:55:58 +0200

    add address table in db

--- fast-application/core/models/user.py
+++ fast-application/core/models/user.py
@@ -1,4 +1,6 @@
-from sqlalchemy.orm import Mapped
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, relationship
 from sqlalchemy.orm import mapped_column
 from sqlalchemy import true
 
@@ -6,11 +8,18 @@ from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
 from .mixins.created_at import CreatedAtMixin
 
+if TYPE_CHECKING:
+    from core.models import Address
+
 
 class User(IntIdPkMixin, CreatedAtMixin, Base):
     username: Mapped[str] = mapped_column(unique=True)
 
     is_active: Mapped[bool] = mapped_column(default=True, server_default=true())
+
+    addresses: Mapped[list["Address"]] = relationship(
+        back_populates="user",
+    )
 
     @property
     def email(self) -> str:
